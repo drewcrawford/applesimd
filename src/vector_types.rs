c099499dd1f85b45083df88e4300e6c774acfbab
Drew Crawford <drew@sealedabstract.com>
2022-06-25 20:58:42 -0500

    Remove algorithms
    Turns out we mostly want to use this for memory layout,
    not rely on apple's implementations
    
    mt2-329

--- src/vector_types.rs
+++ src/vector_types.rs
@@ -1,6 +1,4 @@
-use std::arch::aarch64::{vminvq_u32, vrsqrte_f32, vrsqrts_f32};
 use std::mem::MaybeUninit;
-use std::ops::{BitAnd, BitOr, Neg};
 
 #[derive(Copy,Clone,Debug)]
 #[repr(C, align(16))]
@@ -11,22 +9,10 @@ pub struct Int3 {
     w: MaybeUninit<i32>,
 }
 impl Int3 {
-    #[inline] const fn new(x: i32, y: i32, z: i32) -> Self {
+    #[inline] pub const fn new(x: i32, y: i32, z: i32) -> Self {
         Self {
             x,y,z,w: MaybeUninit::uninit()
         }
-    }
-    #[inline] fn bitwise_neg(self) -> Self {
-        Self::new(self.x.neg(), self.y.neg(), self.z.neg())
-    }
-    #[inline] fn bitwise_and(self,other: Self) -> Self {
-        Self::new(self.x.bitand(other.x), self.y.bitand(other.y), self.z.bitand(other.z))
-    }
-    #[inline] fn bitwise_or(self, other: Self) -> Self {
-        Self::new(self.x.bitor(other.x), self.y.bitor(other.y), self.z.bitor(other.z))
-    }
-    #[inline] fn bitselect(self, y: Self, mask: Self) -> Self {
-        self.bitwise_and(mask.bitwise_neg()).bitwise_or(y.bitwise_and(mask))
     }
 }
 
@@ -71,22 +57,6 @@ impl Float2 {
     }
     #[inline] pub const fn y(&self) -> f32 {
         unsafe{self.y.assume_init()}
-    }
-    #[inline] pub fn fast_rsqrt(self) -> Self {
-        unsafe {
-            let r = vrsqrte_f32(std::mem::transmute(self));
-            let r_rust: Float2 = std::mem::transmute(r);
-            let r_squared = r_rust.elementwise_mult(r_rust);
-            let t = vrsqrts_f32(std::mem::transmute(self), std::mem::transmute(r_squared));
-            let t_rust: Float2 = std::mem::transmute(t);
-            r_rust.elementwise_mult(t_rust)
-        }
-    }
-    #[inline] pub fn elementwise_mult(self,other: Self) -> Self {
-        Float2 {
-            x: self.x * other.x,
-            y: MaybeUninit::new(unsafe{self.y.assume_init() * other.y.assume_init()}),
-        }
     }
 }
 
@@ -137,64 +107,7 @@ impl Float3 {
     }
 
 
-    ///The * operator in simd.
-    #[inline] pub fn elementwise_mult(self, other: Float3) -> Self {
-        Float3::new(self.x * other.x, self.y * other.y, self.z * other.z)
-    }
-    #[inline] pub fn elementwise_mult_scalar(self, other: f32) -> Self {
-        Float3::new(self.x * other, self.y * other, self.z * other)
-    }
 
-    #[inline] pub fn elementwise_sub(self, other: Float3) -> Self {
-        Float3::new(self.x - other.x, self.y - other.y, self.z - other.z)
-    }
-    #[inline] pub fn elementwise_add(self, other: Float3) -> Self {
-        Float3::new(self.x + other.x, self.y + other.y, self.z + other.z)
-    }
-    #[inline] pub fn bitselect(self, y: Float3, mask: Int3) -> Float3 {
-        unsafe {
-            let x: Int3 = std::mem::transmute(self);
-            let y: Int3 = std::mem::transmute(y);
-            let result = x.bitselect(y,mask);
-            let r_f3: Float3 = std::mem::transmute(result);
-            r_f3
-        }
-    }
-    #[inline] pub fn abs(self) -> Float3 {
-        let zero = Float3::ZERO;
-        zero.bitselect(self, Int3::new(0x7fffffff, 0x7fffffff, 0x7fffffff))
-    }
-    #[inline] pub fn max(self) -> f32 {
-        self.x.max(self.y.max(self.z))
-    }
-
-    #[inline] pub fn reduce_add(self) -> f32 {
-        self.x + self.y + self.z
-    }
-
-    ///simd_dot
-    #[inline] pub fn dot(self,other: Self) -> f32 {
-        self.elementwise_mult(other).reduce_add()
-    }
-
-    #[inline] pub fn length_squared(self) -> f32 {
-        self.dot(self)
-    }
-
-    #[inline] pub fn fast_normalize(self) -> Self {
-        let length = self.length_squared();
-        let rsqrd = unsafe {
-            let extend = Float2::from_undef(length);
-            extend.fast_rsqrt().x
-        };
-        self.elementwise_mult_scalar( rsqrd)
-    }
-    #[inline] pub fn fast_length(self) -> f32 {
-        self.precise_length() //odd, I know!??
-    }
-    #[inline] pub fn precise_length(self) -> f32 {
-        self.length_squared().sqrt()
-    }
 }
 
 /// simd_float4
@@ -241,57 +154,4 @@ impl Float4 {
     #[inline] pub fn w_mut(&mut self) -> &mut f32 {
         &mut self.w
     }
-    #[inline] pub fn fast_length(self) -> f32 {
-        self.precise_length() //odd, I know!??
-    }
-    #[inline] pub fn precise_length(self) -> f32 {
-        self.length_squared().sqrt()
-    }
-    #[inline] pub fn length_squared(self) -> f32 {
-        self.dot( self)
-    }
-    #[inline] pub fn dot(self, other: Self) -> f32 {
-        self.elementwise_mult(other).reduce_add()
-    }
-    #[inline] pub fn elementwise_mult(self,other: Self) -> Self {
-        Self::new(self.x * other.x, self.y * other.y, self.z * other.z, self.w * other.w)
-    }
-    #[inline] pub fn elementwise_mult_scalar(self, scalar: f32) -> Self {
-        Self::new(self.x * scalar, self.y * scalar, self.z * scalar, self.w * scalar)
-    }
-    #[inline] pub fn reduce_add(self) -> f32 {
-        self.x + self.y + self.z + self.w
-    }
-    #[inline] pub fn fast_normalize(self) -> Self {
-        let length = self.length_squared();
-        let rsqrd = unsafe {
-            let extend = Float2::from_undef(length);
-            extend.fast_rsqrt().x
-        };
-        self.elementwise_mult_scalar(rsqrd)
-    }
-}
-
-
-#[test] fn test_rsqrt() {
-    let float2 = Float2::new(1.0, 25.0);
-    let sqrt = float2.fast_rsqrt();
-    assert!((sqrt.x() - 1.0).abs() < 0.1);
-    assert!((sqrt.y() - 0.2).abs() < 0.1);
-}
-
-#[test] fn test_normalize() {
-    let norm = Float3::new(1.0, 2.0, 3.0).fast_normalize();
-    assert!((norm.x() - 0.26).abs() < 0.1);
-    assert!((norm.y() - 0.53).abs() < 0.1);
-    assert!((norm.z() - 0.80).abs() < 0.1);
-}
-
-#[test] fn test_abs() {
-    let a = Float3::new(0.0, -20.0, 13370000.0);
-    let abs = a.abs();
-    assert_eq!(a.x.abs(),abs.x);
-    assert_eq!(a.y.abs(),abs.y);
-    assert_eq!(a.z.abs(),abs.z);
-
 }
